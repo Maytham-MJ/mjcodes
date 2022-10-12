1d5d223af966c70aa58b8de5157227f74b546488
M.J <89101458+WarDust00@users.noreply.github.com>
2022-10-13 00:15:19 +0300

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,98 @@
+var youtube = "";
+var twitter = "";
+var password = "fkcodes";
+var linkedin = "";
+var instagram = "";
+var github = "https://github.com/WarDust00";
+var email = "maythamlucky@gmail.com";
+var mortgageQualifier ="https://github.com/WarDust00/Mortgage-Qualifier"
+var mirrorToekn="https://github.com/WarDust00/Mirror-Token-"
+
+whois = [
+  "<br>",
+  "Hey, I'm Maytham AKA M.J!👋",
+  "I'm a software engineer from CA, who builds software applications && cool stuff like this one.",
+  "After graduating from collage, I quickly came to find out that software engineering && building ",
+  "cool things is my passion. I then quickly started to learn and taught myself coding and programming",
+  "languages such as: HTML, CSS, JS, PY, _CommandLine and more.",
+  "I start enjoying the feeling of building things. I than decided to join a bootcamp to further educate",
+  "and build my knowledge. I was trained on Java and how to be a professional Java developer on",
+  "an enterprise level, building  web applications for Fortune 500 companies.",
+
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+ // 'youtube        <a href="' + youtube + '" target="_blank">youtube/WarDust00' + "</a>",//
+ // 'twitter        <a href="' + twitter + '" target="_blank">twitter/M.J' + '</a>',//
+ // 'linkedin       <a href="' + linkedin + '" target="_blank">' + "</a>",
+// 'instagram      <a href="' + instagram + '" target="_blank">instagram/M.J' + '</a>',//
+  'github         <a href="' + github + '" target="_blank">github/WarDust00' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  " I have build few cool projects some are simple just for fun, while others are solving real world need.",
+  " Most projects are on GitHub.",
+  `<br>`,
+  'Project <a href="' + mortgageQualifier + '" target="_blank">Mortgage Qualifier' + '</a>',
+  'Project <a href="' + mirrorToekn + '" target="_blank">Mirror Token' + '</a>',
+
+
+
+  "<br>"
+
+
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is M.J?',
+  '<span class="command">whoami</span>         Who are you?',
+  //'<span class="command">video</span>          View YouTube videos',//
+  '<span class="command">social</span>         Display social Links',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Maytham Web Terminal (M.J) Not A Corporation. All rights reserved.</span>',
+
+"      _=|_   ",
+"    _[_## ]_ ",
+"  _+[_[_+_]P/_     |_       _____       =--|-~",
+"   -\_I_I_[=\--   ~~ [o] ==- |##==]-=-~~ o]H",
+"-~/[_[_|_]_]\\   -_[[=]]    |====]  __  !j]H",
+"  //          \\\   ^U-U^  - |    - ~ .~  U/",
+"~~--__~~~--__~~-__ H_H_   _     --   _H_~",
+"-. _  ~~~#######~~~     ~~~-    ~~--  ._ - ~~-=",
+"   ~~~=~~  -~~--  _     . -      _ _ -",
+"----------------------------------",
+"|     ON June, 20th, 1969          |",
+"|  Here Men from the Planet Earth  |",
+"|   First set Foot upon the Moon   |",
+"| We came in Peace for all Mankind |",
+"---------------------------=2022=--",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
