b1ba4cd9f2657a257e985d299247c72dd5fef747
M.J <89101458+WarDust00@users.noreply.github.com>
2022-10-14 14:45:56 -0700

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -7,6 +7,7 @@ var github = "https://github.com/WarDust00";
 var email = "maythamlucky@gmail.com";
 var mortgageQualifier ="https://github.com/WarDust00/Mortgage-Qualifier"
 var mirrorToekn="https://github.com/WarDust00/Mirror-Token-"
+var CyberBank="https://github.com/WarDust00/Cyber-Bank"
 
 whois = [
   "<br>",
@@ -51,6 +52,8 @@ projects = [
   `<br>`,
   'Project <a href="' + mortgageQualifier + '" target="_blank">Mortgage Qualifier' + '</a>',
   'Project <a href="' + mirrorToekn + '" target="_blank">Mirror Token' + '</a>',
+  'Project <a href="' + CyberBank+ '" target="_blank">CyberBank' + '</a>',
+  
 
 
 
