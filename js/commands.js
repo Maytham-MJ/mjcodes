0b3aebfc497d47bd7b1413194f3337fef0b43f20
M.J <wardust007@gmail.com>
2022-12-24 08:10:03 -0800

    update

--- js/commands.js
+++ js/commands.js
@@ -16,12 +16,19 @@ whois = [
   "I'm a software engineer from CA, who builds software applications && cool stuff like this one.",
   "After graduating from college, I quickly came to find out that software engineering && building ",
   "cool things are my passion. I then quickly started to learn and taught myself coding and programming",
-  "languages such as: HTML, CSS, JS, PY, _CommandLine and more.",
-  "I start enjoying the feeling of building things. I then decided to join a bootcamp to further educate",
-  "and build my knowledge. I was trained on Java and how to be a professional Java developer on",
-  "an enterprise level, building  web applications for Fortune 500 companies.",
+  "languages such as: Java, Solidity, HTML, CSS, JavaScript, GIT, Angular and more.",
+  "I started to enjoy the feeling of building things. I then decided to join a bootcamp to further educate myself",
+  "where I was trained on C# .NET FrameWork, SQL, AZURE, ASP.NET, ASP Web API, and more.",
+
+  "Rrecently, I earned Harvard CS50  Certificate where I learned computer science and software engineering fundemantales.",
+  "I have successfully built an Employee Reimbursement System using an ASP.NET Core Web API written in C# as well as 2 console applications in Java.",
+  "One is Mortgage Qualifier and the other one is Cyber Bank. My proficiency in multiple languages and work knowledge allowed me to have excellent",
+  "communication and teamwork skills. In my free time, I enjoy swimming, playing soccer, and staying up-to-date on the latest automotive trends.",
+  
 
   "<br>"
+
+
 ];
 
 whoami = [
